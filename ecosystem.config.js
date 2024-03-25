71732f33f20d6f6fb2634351068c2000fb88316d
haamym <jenrid@live.com>
2024-03-25 14:29:55 +0500

    pm2 changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "index.js",
       //  configuration
       autorestart: false, // Important: Disable autorestart for cron jobs
-      cron_restart: "*/2 * * * *", // change to "0 20 * * *" - "At 20:00" (8 PM) Maldives time (UTC+5)
+      cron_restart: "0 20 * * *", // change to "0 20 * * *" - "At 20:00" (8 PM) Maldives time (UTC+5)
       env: {
         NODE_ENV: "production",
         TZ: "Indian/Maldives", //  timezone to Maldives Time
